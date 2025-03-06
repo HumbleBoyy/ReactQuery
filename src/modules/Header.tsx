52f58be728ae407d3e07cdd404cd02d358511651
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-06 18:20:20 +0500

    New Commit

--- src/modules/Header.tsx
+++ src/modules/Header.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <header className='flex justify-between items-center p-5 bg-blue-700 shadow-2xl'>
+      <h1 className='text-[25px] font-semibold text-white'>Purchase</h1>
+    </header>
   )
 }
 
