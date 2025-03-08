2c6d2b405860076d30520a92135539ac5e2097f5
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-08 07:03:05 +0500

    New Commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,26 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+    defaultOptions:{
+       queries:{
+          retry:1,
+          staleTime:1000 * 60 * 5,
+          // @ts-ignore
+          cacheTime:1000 * 60 * 5,
+          refetchOnWindowFocus:false
+       }
+    }
+})
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-     <App />
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
   </BrowserRouter>
   
 )
--- src/modules/Header.tsx
+++ src/modules/Header.tsx
@@ -1,9 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { instance } from '../hooks/instance'
 
 const Header = () => {
+  const {data:categoryList = []} = useQuery({
+    queryKey:["categories"],
+    queryFn:()=> instance().get("/categories").then(res => res.data)
+  })
+  console.log(categoryList)
+
+  
   return (
     <header className='flex justify-between items-center p-5 bg-blue-700 shadow-2xl'>
       <h1 className='text-[25px] font-semibold text-white'>Purchase</h1>
+
     </header>
   )
 }
